33e95d3cddc4d4fc35811ac3e27eb936d0ffe03c
Камиль Юмагулов <Kamil@dengimigom.local>
2024-01-29 16:23:24 +0300

    first commit

---
+++ js/choices.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const element = document.querySelector('#material');
+  const choices = new Choices(element, {
+    searchEnabled: false
+  });
+
+});
+document.addEventListener("DOMContentLoaded", function() {
+  const element = document.querySelector('#material2');
+  const choices = new Choices(element, {
+    searchEnabled: false
+  });
+
+});
